4b6add2dc810e1b572a826149499eaae13589925
umang <umang.agrawal@couchbase.com>
2021-01-19 20:50:01 +0530

    Added cbas queries for catapult doc template
    Change-Id: I72c01dc473b1286bd4845f61695e4484f6ef9bd7

--- Query/cbas_queries.py
+++ Query/cbas_queries.py
@@ -43,5 +43,9 @@ cbas_queries = {
     "select result,SUM(rating) from {0} where result is not null and rating >= 410 and rating <= 420 group by result",
     "select round(min(rating),2) as min_rating,round(max(rating),2) as max_rating,round(avg(rating),2) as avg_rating from {0} where result is not null and rating between 500 and 520]"
   ],
-  "catapult_queries": []
+  "catapult_queries": [
+    "select meta().id from {0} where country is not null and `type` is not null and (any r in reviews satisfies r.ratings.`Check in / front desk` is not null end) limit 100 ",
+    "select avg(price) as AvgPrice, min(price) as MinPrice, max(price) as MaxPrice from keyspacenameplaceholder where free_breakfast=True and free_parking=True and price is not null and array_count(public_likes)>5 and `type`='Hotel' group by country limit 100",
+    "select city,country,count(*) from keyspacenameplaceholder where free_breakfast=True and free_parking=True group by country,city order by country,city limit 100 offset 100"
+  ]
 }
