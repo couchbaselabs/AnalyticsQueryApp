0fc6fe0c322ce1c8fac4028551247403100c0826
umang <umang.agrawal@couchbase.com>
2021-02-19 14:39:17 +0530

    Fixed a failing query
    Change-Id: I95221ff564c2b3302b1ffba002e8ccce7e6f6f2a

--- Query/cbas_queries.py
+++ Query/cbas_queries.py
@@ -6,7 +6,7 @@ cbas_queries = {
     "select result,AVG(rating),MIN(rating),MAX(rating) from {0}.{1} where result is not missing and rating >= 400 and rating <= 1000 group by result",
     "select result,SUM(rating) from {0}.{1} where result is not null and rating >= 115 and rating <= 125 group by result",
     "select result,SUM(rating) from {0}.{1} where result is not null and rating >= 410 and rating <= 420 group by result",
-    "select round(min(rating),2) as min_rating,round(max(rating),2) as max_rating,round(avg(rating),2) as avg_rating from {0}.{1} where result is not null and rating between 500 and 520]"
+    "select round(min(rating),2) as min_rating,round(max(rating),2) as max_rating,round(avg(rating),2) as avg_rating from {0}.{1} where result is not null and rating between 500 and 520"
   ],
   "catapult_queries": [
     "select meta().id from {0}.{1} where country is not null and `type` is not null and (any r in {1}.reviews satisfies r.ratings.`Check in / front desk` is not null end) limit 100",
